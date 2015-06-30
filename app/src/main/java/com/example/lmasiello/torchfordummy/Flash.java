f263e9e7174c6dd23e266a4759175fa0a52ebdd6
jardelxever <danilo.masiello@gmail.com>
2015-06-30 23:40:53 +0200

    versione 30_06_2015

--- app/src/main/java/com/example/lmasiello/torchfordummy/Flash.java
+++ app/src/main/java/com/example/lmasiello/torchfordummy/Flash.java
@@ -48,27 +48,30 @@ public class Flash extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_flash, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+    //    getMenuInflater().inflate(R.menu.menu_flash, menu);
+    //    return true;
+    //}
+
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //    int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    //    if (id == R.id.action_settings) {
+    //        return true;
+    //    }
 
-        return super.onOptionsItemSelected(item);
-    }
+    //    return super.onOptionsItemSelected(item);
+    //}
     void init(){
         try {
             mCameraManager = (CameraManager) Flash.this.getSystemService(Context.CAMERA_SERVICE);
