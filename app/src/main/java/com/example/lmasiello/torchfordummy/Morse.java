4c283231afe53c363f2f7226311a3c2cb36a854c
lmasiello <danilo.masiello@gmail.com>
2015-07-03 13:49:05 +0200

    rev 03_07_2015 13_48 (lavoro)

--- app/src/main/java/com/example/lmasiello/torchfordummy/Morse.java
+++ app/src/main/java/com/example/lmasiello/torchfordummy/Morse.java
@@ -5,8 +5,243 @@ package com.example.lmasiello.torchfordummy;
  */
 public class Morse {
     private String A = ".-";
+    private String B = "-...";
+    private String C = "-.-.";
+    private String D = "-..";
+    private String E = ".";
+    private String F = "..-.";
+    private String G = "--.";
+    private String H = "....";
+    private String I = "..";
+    private String J = ".---";
+    private String K = "-.-";
+    private String L = ".-..";
+    private String M = "--";
+    private String N = "-.";
+    private String O = "---";
+    private String P = ".--.";
+    private String Q = "--.-";
+    private String R = ".-.";
+    private String S = "...";
+    private String T = "-";
+    private String U = "..-";
+    private String V = "...-";
+    private String W = ".--";
+    private String X = "-..-";
+    private String Y = "-.--";
+    private String Z = "--..";
+    private String zero = "-----";
+    private String uno = ".----";
+    private String due = "..---";
+    private String tre = "...--";
+    private String quattro = "....-";
+    private String cinque = ".....";
+    private String sei = "-....";
+    private String sette = "--...";
+    private String otto = "---..";
+    private String nove = "----.";
+    private String punto = ".-.-.-";
+    private String virgola = "-..--";
+    private String duepunti = "---...";
+    private String puntointerrogativo = "..--..";
+    private String uguale = "-...-";
+    private String meno = "-....-";
+    private String tondaaperta = "-.--.";
+    private String tondachiusa = "-.--.-";
+    private String virgolette = ".-..-.";
+    private String apice = ".----.";
+    private String slash = "-..-.";
+    private String chiocciola = ".--.-.";
 
     public String getA() {
         return A;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public String getF() {
+        return F;
+    }
+
+    public String getG() {
+        return G;
+    }
+
+    public String getH() {
+        return H;
+    }
+
+    public String getI() {
+        return I;
+    }
+
+    public String getJ() {
+        return J;
+    }
+
+    public String getK() {
+        return K;
+    }
+
+    public String getL() {
+        return L;
+    }
+
+    public String getM() {
+        return M;
+    }
+
+    public String getN() {
+        return N;
+    }
+
+    public String getO() {
+        return O;
+    }
+
+    public String getP() {
+        return P;
+    }
+
+    public String getQ() {
+        return Q;
+    }
+
+    public String getR() {
+        return R;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public String getT() {
+        return T;
+    }
+
+    public String getU() {
+        return U;
+    }
+
+    public String getV() {
+        return V;
+    }
+
+    public String getW() {
+        return W;
+    }
+
+    public String getX() {
+        return X;
+    }
+
+    public String getY() {
+        return Y;
+    }
+
+    public String getZ() {
+        return Z;
+    }
+
+    public String getUno() {
+        return uno;
+    }
+
+    public String getZero() {
+        return zero;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public String getTre() {
+        return tre;
+    }
+
+    public String getQuattro() {
+        return quattro;
+    }
+
+    public String getCinque() {
+        return cinque;
+    }
+
+    public String getSei() {
+        return sei;
+    }
+
+    public String getSette() {
+        return sette;
+    }
+
+    public String getOtto() {
+        return otto;
+    }
+
+    public String getNove() {
+        return nove;
+    }
+
+    public String getPunto() {
+        return punto;
+    }
+
+    public String getVirgola() {
+        return virgola;
+    }
+
+    public String getDuepunti() {
+        return duepunti;
+    }
+
+    public String getPuntointerrogativo() {
+        return puntointerrogativo;
+    }
+
+    public String getUguale() {
+        return uguale;
+    }
+
+    public String getMeno() {
+        return meno;
+    }
+
+    public String getTondaaperta() {
+        return tondaaperta;
+    }
+
+    public String getTondachiusa() {
+        return tondachiusa;
+    }
+
+    public String getVirgolette() {
+        return virgolette;
+    }
+
+    public String getApice() {
+        return apice;
+    }
+
+    public String getSlash() {
+        return slash;
+    }
+
+    public String getChiocciola() {
+        return chiocciola;
     }
 }
